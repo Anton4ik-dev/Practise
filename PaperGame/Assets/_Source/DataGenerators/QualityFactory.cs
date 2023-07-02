291d4440ea2af8c70706edd62430559c9242bb53
Anton4ik-dev <afedot2004@gmail.com>
2023-07-02 19:41:20 +0300

    UI changes

--- PaperGame/Assets/_Source/DataGenerators/QualityFactory.cs
+++ PaperGame/Assets/_Source/DataGenerators/QualityFactory.cs
@@ -15,7 +15,7 @@ namespace DataFactories
 
             if (isError == true && _canBeWithMistake == true)
             {
-                for (int i = 0; i < Random.Range(0, scratches.Count); i++)
+                for (int i = 0; i <= Random.Range(0, scratches.Count); i++)
                     scratches[i].SetActive(true);
                 return false;
             }
